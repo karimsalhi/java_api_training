aca4661274422f0581a66e5cafba3b99f4f2a5f9
karimsalhi <karim.salhi@epita.fr>
2022-03-27 22:55:29 +0200

    Corrected test ping

--- src/test/java/fr/lernejo/server/ServerTest.java
+++ src/test/java/fr/lernejo/server/ServerTest.java
@@ -7,6 +7,11 @@ import java.io.IOException;
 
 public class ServerTest {
     @Test
+    public void testServer_whenRequestHasBeenAccepted_ShouldReturnCodeStatus200() throws IOException, InterruptedException {
+        Server server = new Server(8000, "localhost");
+        Assertions.assertTrue(server.run());
+    }
+
     public void testPingRequestOnServer_whenRequestHasBeenAccepted_ShouldReturnCodeStatus200() throws IOException, InterruptedException {
         Server server = new Server(8000, "localhost");
         Assertions.assertTrue(server.run());
